80287fda967f9810ec36abaf82a36c4520d33bb9
SimoPrG <SimoPrG@users.noreply.github.com>
2015-03-16 16:35:59 +0200

    Attributes

---
+++ DefiningClassesPart2/Attributes/AttributesMain.cs
@@ -0,0 +1,63 @@
+﻿/*Problem 11. Version attribute
+
+    Create a [Version] attribute that can be applied to structures, classes, interfaces, enumerations and methods and holds
+    a version in the format major.minor (e.g. 2.11).
+    Apply the version attribute to a sample class and display its version at runtime.
+*/
+namespace Attributes
+{
+    using System;
+    using System.Reflection;
+
+    [VersionAttribute(0, 9)]
+    public class AttributesMain
+    {
+        [VersionAttribute(1, 1)]
+        public static void Main()
+        {
+            GetAttribute(typeof(AttributesMain));
+        }
+
+        [VersionAttribute(2, 1)]
+        public static void GetAttribute(Type t)
+        {
+            VersionAttribute att;
+
+            // Get the class-level attributes. 
+
+            // Put the instance of the attribute on the class level in the att object.
+            att = (VersionAttribute)Attribute.GetCustomAttribute(t, typeof(VersionAttribute));
+
+            if (att == null)
+            {
+                Console.WriteLine("No attribute in class {0}.\n", t.ToString());
+            }
+            else
+            {
+                Console.WriteLine("The version of the class {0} is {1}.{2}", t.ToString(), att.Major, att.Minor);
+            }
+
+            // Get the method-level attributes. 
+
+            // Get all methods in this class, and put them 
+            // in an array of System.Reflection.MemberInfo objects.
+            MemberInfo[] MyMemberInfo = t.GetMethods();
+
+            // Loop through all methods in this class that are in the 
+            // MyMemberInfo array. 
+            for (int i = 0; i < MyMemberInfo.Length; i++)
+            {
+                att = (VersionAttribute)Attribute.GetCustomAttribute(MyMemberInfo[i], typeof(VersionAttribute));
+                if (att == null)
+                {
+                    Console.WriteLine("No attribute in member function {0}.\n", MyMemberInfo[i].ToString());
+                }
+                else
+                {
+                    Console.WriteLine("The version of the method {0} is {1}.{2}",
+                        MyMemberInfo[i].ToString(), att.Major, att.Minor);
+                }
+            }
+        }
+    }
+}
---
+++ DefiningClassesPart2/Attributes/VersionAttribute.cs
@@ -0,0 +1,26 @@
+﻿/*Problem 11. Version attribute
+
+    Create a [Version] attribute that can be applied to structures, classes, interfaces, enumerations and methods and holds
+    a version in the format major.minor (e.g. 2.11).
+    Apply the version attribute to a sample class and display its version at runtime.
+*/
+
+namespace Attributes
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Struct | AttributeTargets.Class | AttributeTargets.Interface
+    | AttributeTargets.Enum | AttributeTargets.Method, AllowMultiple = false)]
+    public class VersionAttribute : System.Attribute
+    {
+        public VersionAttribute(int major, int minor)
+        {
+            this.Major = major;
+            this.Minor = minor;
+        }
+
+        public int Major { get; private set; }
+
+        public int Minor { get; private set; }
+    }
+}
