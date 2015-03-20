854afe03ba6346e22dabf5bd8a36b0e27d3c0e78
SimoPrG <SimoPrG@users.noreply.github.com>
2015-03-20 21:15:32 +0200

    Timer

---
+++ ExtensionMethodsDelegatesLambdaLINQ/Timer/Timer.cs
@@ -0,0 +1,60 @@
+﻿/*Problem 7. Timer
+
+    Using delegates write a class Timer that can execute certain method at each t seconds.
+*/
+
+namespace Timer
+{
+    using System;
+    using System.Threading;
+
+    public class Timer
+    {
+        private const int AThousandMilliseconds = 1000; // One second is 1000 miliseconds
+        private int seconds;
+
+        public Timer(params TimerDlg[] methods) : this(1, methods)
+        {
+        }
+
+        public Timer(int seconds, params TimerDlg[] methods)
+        {
+            this.Seconds = seconds;
+            foreach (var method in methods)
+            {
+                this.Methods += method;
+            }
+        }
+
+        public delegate void TimerDlg();
+
+        private TimerDlg Methods { get; set; }
+
+        public int Seconds
+        {
+            get
+            {
+                return this.seconds;
+            }
+
+            set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentException("The time interval cannot be less than 1.");
+                }
+
+                this.seconds = value;
+            }
+        }
+
+        public void Start()
+        {
+            while (true)
+            {
+                this.Methods();
+                Thread.Sleep(this.Seconds * AThousandMilliseconds);
+            }
+        }
+    }
+}
---
+++ ExtensionMethodsDelegatesLambdaLINQ/Timer/TimerMain.cs
@@ -0,0 +1,25 @@
+﻿/*Problem 7. Timer
+
+    Using delegates write a class Timer that can execute certain method at each t seconds.
+*/
+
+namespace Timer
+{
+    using System;
+
+    public class TimerMain
+    {
+        public static void Main()
+        {
+            // I have extended this task a little bit. The constructor of the Timer class can take multiple arguments which are methods.
+            // In this example the first method is predifined, the second is a lamda, and the third is anonymous method.
+            Timer timer = new Timer(2, PrintSomething, () => Console.WriteLine("there!"), delegate() { Console.WriteLine("How are you?"); });
+            timer.Start();
+        }
+
+        public static void PrintSomething()
+        {
+            Console.Write("Hello, ");
+        }
+    }
+}
