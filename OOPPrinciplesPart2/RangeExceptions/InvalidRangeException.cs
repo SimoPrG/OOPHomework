086b6355efe65de7c251fc548c9f4aa0b8ca3daa
SimoPrG <SimoPrG@users.noreply.github.com>
2015-03-29 05:56:45 +0300

    Problem 3. Range Exceptions

---
+++ OOPPrinciplesPart2/RangeExceptions/InvalidRangeException.cs
@@ -0,0 +1,26 @@
+﻿//Define a class InvalidRangeException<T> that holds information about an error condition related to invalid range.
+//It should hold error message and a range definition [start … end].
+
+namespace RangeExceptions
+{
+    using System;
+
+    public class InvalidRangeException<T> : ApplicationException
+    {
+        public InvalidRangeException(string message, T startOfRange, T endOfRange)
+            : this(message, startOfRange, endOfRange, null)
+        {
+        }
+
+        public InvalidRangeException(string message, T startOfRange, T endOfRange, Exception innerException)
+            : base(message, innerException)
+        {
+            this.StartOfRange = startOfRange;
+            this.EndOfRange = endOfRange;
+        }
+
+        public T StartOfRange { get; private set; }
+
+        public T EndOfRange { get; private set; }
+    }
+}
---
+++ OOPPrinciplesPart2/RangeExceptions/InvalidRangeExceptionTest.cs
@@ -0,0 +1,44 @@
+﻿/*Problem 3. Range Exceptions
+
+    Define a class InvalidRangeException<T> that holds information about an error condition related to invalid range.
+    It should hold error message and a range definition [start … end].
+    Write a sample application that demonstrates the InvalidRangeException<int> and InvalidRangeException<DateTime>
+    by entering numbers in the range [1..100] and dates in the range [1.1.1980 … 31.12.2013].
+*/
+
+namespace RangeExceptions
+{
+    using System;
+    using System.Collections.Generic;
+
+    class InvalidRangeExceptionTest
+    {
+        static void Main()
+        {
+            const int lowInt = 1;
+            const int highInt = 100;
+
+            DateTime lowDate = new DateTime(1980, 1, 1);
+            DateTime highDate = new DateTime(2013, 12, 31);
+
+            TryCatch(lowInt, highInt, 42);
+
+            TryCatch(lowDate, highDate, new DateTime(1989, 11, 9));
+        }
+
+        private static void TryCatch<T>(T low, T high, T variable)
+        {
+            try
+            {
+                if (Comparer<T>.Default.Compare(low, variable) <= 0 && Comparer<T>.Default.Compare(variable, high) <= 0)
+                {
+                    throw new InvalidRangeException<T>("The value is in the invalid range", low, high);
+                }
+            }
+            catch (InvalidRangeException<T> ex)
+            {
+                Console.WriteLine("{0}: {1} - {2}", ex.Message, ex.StartOfRange, ex.EndOfRange);
+            }
+        }
+    }
+}
